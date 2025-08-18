6d31870aea65677ca2df3b1213c75c826b629f85
mhernangonzalez32-gif <mhernangonzalez32@gmail.com>
2025-08-18 11:49:46 -0300

    primer commit

---
+++ Unidad 1/Eje 4.py
@@ -0,0 +1,7 @@
+radio = float(input("Ingrese el radio de un circulo "))
+
+area = 3.14 * radio**2
+perimetro = 2 * 3.14 * radio
+
+print(f"El area es {area}")
+print(f"El perimetro es : {perimetro}")
---
+++ Unidad 1/ejercicio 3.py
@@ -0,0 +1,6 @@
+nombre = input ("Ingrese su numbre ")
+apellido = input("Ingrese su apellido ")
+edad = input(f"Ingrese su edad {nombre} ")
+nacionalidad = input("Ingrese la localidad donde vive ")
+
+print(f"Soy {nombre} {apellido}, tengo {edad} años, y vivo en {nacionalidad} ")
