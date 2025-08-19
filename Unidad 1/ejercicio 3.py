3036bf3e11f3946b7915ff330e92667a63de2f6f
mhernangonzalez32-gif <mhernangonzalez32@gmail.com>
2025-08-19 14:23:28 -0300

    borrado archivo vasio

--- Unidad 1/ejercicio 3.py
+++
@@ -1,7 +0,0 @@
-
-primer_numero = int(input("Ingrese un numero natural "))
-segundo_numero = int(input("Ingrese el segundo numero natural "))
-tercer_numero = int(input("Ingrese el segundo numero natural "))
-
-promedio = (primer_numero + segundo_numero + tercer_numero) // 3                    
-print(f"El promedio de estos numero es: {promedio}")                  
