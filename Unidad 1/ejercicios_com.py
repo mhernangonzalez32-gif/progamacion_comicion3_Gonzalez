43c441586fac887b9ddb1517335b2100f1a6aa59
mhernangonzalez32-gif <mhernangonzalez32@gmail.com>
2025-08-20 22:04:02 -0300

    mediana mente terminado

--- Unidad 1/ejercicios_com.py
+++ Unidad 1/ejercicios_com.py
@@ -1,1 +1,99 @@
-git add . 
+print("----Ejercicio 1")
+
+numero1 = int(19)
+print(numero1)
+
+print("----Ejercicio 2")
+
+numero2 = int(23)
+print(numero2)
+
+print("----Ejercicio 3")
+
+suma = numero1 + numero2
+print(f"La suma de estos numero es {suma}")
+
+print("----Ejercicio 4")
+
+resta = numero1 - numero2 
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print(f"Resta: {resta} \nMultiplicacion: {multiplicacion} \nDivision: {division}")
+
+print("----Ejercicio 5")
+
+nombre = str("Hernan")
+print(nombre)
+
+print("----Ejercicio 6")
+
+precio = 20.500
+print(precio)
+
+print("----Ejercicio 7")
+
+descuento = 0.10
+print(f"El descuento es de: {descuento}")
+
+print("----Ejercicio 8")
+
+precio_final = precio - (precio * descuento) 
+
+print(f"El precion final es: {precio_final}")
+
+print("----Ejercicio 9")
+
+cadena = str("Cada ejercicio debe mostrarse por pantalla?")
+print(cadena)
+
+print("----Ejercicio 10")
+
+longitud = len(cadena)
+
+print(f"La longitud es: {longitud}")
+
+print("----Ejercicio 11")
+
+precio = int(15.50)
+
+print(precio)
+
+print("----Ejercicio 12")
+
+nombre = "Hernan"
+apellido = "Gonzalez" 
+nombre_completo = f"{nombre} {apellido}"
+
+print(nombre_completo)
+
+print("----Ejercicio 13")
+
+edad = 29
+print(edad)
+edad = edad + 5  
+print(edad) 
+edad = edad - 10  
+print(edad)
+
+print("----Ejercicio 14")
+altura = 1.67
+print(altura)
+altura = altura * 4
+print(altura)
+altura = altura / 3
+print(altura)
+
+print("----Ejercicio 15")
+
+nombre = "HERNAN GONZALEZ"
+print(nombre)
+nombre_minusculas = nombre.lower() 
+print(nombre_minusculas)
+
+print("----Ejercicio 16")
+
+print(nombre)
+nombre_cap = nombre.capitalize()
+print(nombre_cap)
+
