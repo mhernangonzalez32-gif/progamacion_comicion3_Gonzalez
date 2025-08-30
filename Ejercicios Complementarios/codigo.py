c67e9bc159f2ce9db09367ae9d21a8e32a8c8b80
mhernangonzalez32-gif <mhernangonzalez32@gmail.com>
2025-08-30 02:23:30 -0300

    h

---
+++ Ejercicios Complementarios/codigo.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese nombre: ")
+apellido = input("Ingrese apellido: ")
+nombre_completo = str(f"{nombre} {apellido}").title()
+dni = input("Ingrese DNI: ")
+obra_social = ["si","no"]
